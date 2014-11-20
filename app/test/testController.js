857dcfc770aea2bb3d6990f665b79c538ba89466
Omar, working on hooks <omarchacin@gmail.com>
2014-11-20 14:31:47 +0100

    Chat get and post data to the firebase

--- app/test/testController.js
+++ app/test/testController.js
@@ -11,20 +11,85 @@
         /* jshint validthis: true */
         var vm = this;
         var rootRef = new Firebase('https://flickering-torch-9814.firebaseio.com/');
-        var childRef = rootRef.child('message');
+        var messagesRef = rootRef.child('messages');
+        var titleRef = rootRef.child('title');
 
-        vm.activate = activate;
-        vm.title = 'MainController';
+        vm.title = null;
+        vm.currentUser = null;
+        vm.currentText = null;
+        vm.messages = [];
 
-        childRef.on('value', function(snapshot) {
+        titleRef.once('value', function(snapshot) {
+            vm.title = snapshot.val();
+        });
+
+        messagesRef.on('child_added', function(snapshot) {
             $timeout(function() {
                 var snapshotVal = snapshot.val();
                 console.log(snapshotVal);
-                vm.message = snapshot.val();
+                vm.messages.push({
+                    text: snapshotVal.text,
+                    user: snapshotVal.user,
+                    name: snapshot.name()
+                });
+            });
+        });
+
+        messagesRef.on('child_changed', function(snapshot) {
+            $timeout(function() {
+                var snapshotVal = snapshot.val();
+                var message = findMessageByName(snapshot.name());
+                message.text = snapshotVal.text;
+            });
+        });
+
+        messagesRef.on('child_removed', function(snapshot) {
+            $timeout(function() {
+                deleteMessageByName(snapshot.name());
             });
         });
 
 
+        ////////////////
+
+        function findMessageByName (name) {
+            var messageFound = null;
+            for(var i =0; i < vm.messages.length; i++ ) {
+                var currentMessage = vm.messages[i];
+                if (currentMessage.name == name) {
+                    messageFound = currentMessage;
+                    break;
+                }
+            }
+
+            return messageFound;
+        }
+
+        function deleteMessageByName (name) {
+            for(var i =0; i < vm.messages.length; i++ ) {
+                var currentMessage = vm.messages[i];
+                if (currentMessage.name == name) {
+                    vm.messages.splice(i, 1);
+                    break;
+                }
+            }
+        }
+
+        vm.sendMessage = function() {
+          var newMessage = {
+              user: vm.currentUser,
+              text: vm.currentText
+          };
+
+          messagesRef.push(newMessage);
+        };
+
+        vm.turnFeedOff = function() {
+            messagesRef.off();
+        };
+
+
+        /*
         $scope.$watch('vm.message.text', function(newVal) {
             if (!newVal) {
                 return;
@@ -58,6 +123,7 @@
 
         function activate() {
         }
+        */
 
 
     }
