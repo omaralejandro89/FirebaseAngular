34b8df7646df5d52abe780a99656c6fd87cd876a
Omar, working on hooks <omarchacin@gmail.com>
2014-11-20 09:35:29 +0100

    Successful authentication using Firebase and Angular

--- app/core/core.module.js
+++ app/core/core.module.js
@@ -2,7 +2,8 @@
     'use strict';
 
     angular.module('app.core', [
-        'ui.router'
+        'ui.router',
+        'firebase'
     ]);
 
 })();
--- app/home/homeController.js
+++ app/home/homeController.js
@@ -3,21 +3,42 @@
 
     angular
         .module('app.home')
-        .controller('HomeController', HomeController);
+        .controller('HomeController', ['$firebaseSimpleLogin', '$state', HomeController]);
 
 
-    function HomeController() {
+    function HomeController($firebaseSimpleLogin, $state) {
         /* jshint validthis: true */
         var vm = this;
+        var firebaseObj = new Firebase("https://glaring-heat-2792.firebaseio.com/");
+        var loginObj = $firebaseSimpleLogin(firebaseObj);
 
         vm.activate = activate;
         vm.title = 'HomeController';
+        vm.signIn = signIn;
 
         activate();
 
         ////////////////
 
         function activate() {
+        }
+
+        function signIn(event) {
+            event.preventDefault(); // To prevent form refresh
+            var username = vm.user.email;
+            var password = vm.user.password;
+
+            loginObj.$login('password', {
+                email: username,
+                password: password
+            }).then(function (user) {
+                //Success callback
+                console.log("Authentication successful");
+                $state.go('state1')
+            }, function (error) {
+                //Failure callback
+                console.log("Authentication failure");
+            })
         }
 
 
